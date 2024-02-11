11052bffd09bd168d1c7a0ac623ba5d0379907e9
Alex Jiuk Kim <basecamp@Alexs-Mac-mini.local>
2024-02-11 19:20:13 +0900

    .

--- fe/src/app/map/page.tsx
+++ fe/src/app/map/page.tsx
@@ -6,7 +6,9 @@ import BaseMap from "@/components/map";
 export default function Home() {
   return (
     <section className="z-0 flex flex-col items-center justify-center w-full h-screen">
-      <Header title="지도 탐색"></Header>
+      <div className="fixed top-0 h-fit w-full z-50">
+        <Header title="지도 탐색"></Header>
+      </div>
       <BaseMap></BaseMap>
     </section>
   );
